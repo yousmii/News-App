32ab0b81ac0bfe454c1cafd7306a0327be797b03
AchrafYndz <achraf.yandouzi@gmail.com>
2023-03-01 10:17:22 +0100

    Implement RSS parser

---
+++ src/News-App/rss_parser.py
@@ -0,0 +1,29 @@
+import xml.etree.ElementTree as ET
+
+
+def parse(path):
+    html_file = open('templates/articles.html', 'w+')
+
+    tree = ET.parse(path)
+    root = tree.getroot()
+
+    for entry in root:
+        if entry.tag[29:] == "entry":
+            html_file.write("<div class=\"article\">\n")
+            for child in entry:
+                if child.tag[29:] == "link" and child.attrib["type"] in ["image/jpeg", "image/png"]:
+                    html_file.write(f"<img src=\"{child.attrib['href']}\">\n")
+                elif child.tag[29:] == "link" and child.attrib["type"] == "text/html":
+                    html_file.write(f"<a href=\"{child.attrib['href']}\" target='blank'>\n")
+            for child in entry:
+                if child.tag[29:] == "title":
+                    html_file.write(f"<h2>{child.text}</h2>\n")
+            html_file.write("</a>\n</div>\n")
+
+
+def main():
+    parse('static/vrtnieuws.xml')
+
+
+if __name__ == "__main__":
+    main()
