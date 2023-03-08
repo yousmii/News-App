51446805cfdb01b8b3691891232da4c059a1df52
AchrafYndz <achraf.yandouzi@gmail.com>
2023-03-08 15:32:09 +0100

    Make rss_feed parser work with links rather than static xml files

--- src/server/rss_parser.py
+++ src/server/rss_parser.py
@@ -1,10 +1,8 @@
 import feedparser
 
-def parse(path):
+def parse(link):
     # Parse the RSS feed
-    xml_file = open(path)
-    xml_string = xml_file.read()
-    feed = feedparser.parse(xml_string)
+    feed = feedparser.parse(link)
 
     articles = []
 
@@ -37,19 +35,19 @@ def parse(path):
 
 def main():
     print("---vrt---")
-    parse('static/vrtnieuws.xml')
+    parse('https://www.vrt.be/vrtnws/en.rss.articles.xml')
     print("\n")
     print("---gva---")
-    parse('static/gva.xml')
+    parse('https://www.gva.be/rss/section/ca750cdf-3d1e-4621-90ef-a3260118e21c')
     print("\n")
-    print("---hln---")
-    parse('static/hln.xml')
+    print("---het nieuwsblad---")
+    parse('https://www.nieuwsblad.be/rss/section/55178e67-15a8-4ddd-a3d8-bfe5708f8932')
     print("\n")
     print("---de morgen---")
-    parse('static/demorgen.xml')
+    parse('https://www.demorgen.be/in-het-nieuws/rss.xml')
     print("\n")
     print("---sporza---")
-    parse('static/sporza.xml')
+    parse('https://sporza.be/nl.rss.xml')
 
 if __name__ == "__main__":
     main()
