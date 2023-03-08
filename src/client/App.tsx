a0eaaab76d3cd4d1f9581b2d3b4d2521c961bf51
Yousra <yousra.smits@hotmail.com>
2023-03-08 14:56:10 +0100

    Vite react added

---
+++ src/client/App.tsx
@@ -0,0 +1,13 @@
+import { useState } from "react";
+import axios from "axios";
+import Header from "./components/Header";
+
+function App() {
+  return (
+    <div>
+      <Header />
+    </div>
+  );
+}
+
+export default App;
