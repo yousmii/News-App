5ec23ef320b8027c9c6ee40cfbe694344791fc1b
judithher <Judith.Hershko@student.uantwerpen.be>
2023-03-08 23:41:46 +0100

    database kan info uit flask halen

--- src/News-App/app.py
+++ src/News-App/app.py
@@ -1,59 +1,82 @@
 import os
+import uuid
 
 from flask import Flask
 from flask.templating import render_template
 from flask import request, session, jsonify, redirect, flash
-
 from config import config_data
-from DBConnection import DBConnection
+from sql.config import engine
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
 
-from waitress import serve
-
-#from src.ProgDBTutor.config import config_data
-
+# create the extension
+db = SQLAlchemy()
 # INITIALIZE SINGLETON SERVICES
-app = Flask('News-App ')
+app = Flask('News-App')
 app.secret_key = '*^*(*&)(*)(*afafafaSDD47j\3yX R~X@H!jmM]Lwf/,?KT'
 app_data = dict()
 app_data['app_name'] = config_data['app_name']
-connection = DBConnection(dbname=config_data['dbname'], dbuser=config_data['dbuser'], password="")
 
-DEBUG = False
-HOST = "127.0.0.1" if DEBUG else "0.0.0.0"
+# INITIALIZE SINGLETON SERVICES
+#app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///project.db"
+app.config["SQLALCHEMY_DATABASE_URI"] =engine.url
+db.init_app(app)
 
-# TEST USE
+from database import *
+
+# TEST USER
 user = {"username": "abc", "password": "xyz"}
-
 
 # REST API
 # See https://www.ibm.com/developerworks/library/ws-restful/index.html
 
+# get feed
+"""
+@app.route("/admin", methods=["GET", "POST"])
+def get_feed():
+    if request.method == "POST":
+        user = User()
+        user.add(3,request.form["feed_url"])
+        db.session.add(user)
+        db.session.commit()
+        return redirect('/admin')
+
+    return render_template("admin.html")
+"""
+
+
 # Login
-@app.route('/login', methods = ['POST', 'GET'])
+@app.route('/login', methods=['POST', 'GET'])
 def login():
-    if(request.method == 'POST'):
+    if (request.method == 'POST'):
         username = request.form.get('username')
         password = request.form.get('password')
         if username == user['username'] and password == user['password']:
-
             session['user'] = username
             return redirect('/admin')
 
         flash('Wrong password', 'error')
-        #return "<h1>Wrong username or password</h1>"    #if the username or password does not matches
+        # return "<h1>Wrong username or password</h1>"    #if the username or password does not matches
 
     return render_template("login.html")
 
-#Logout
+
+# Logout
 @app.route('/logout')
 def logout():
     session.pop('user')
     return redirect('/')
-
 # VIEW
 @app.route("/")
 def main():
+    user=User(cookie=4000,history="hi")
+    # demo of adding to db
+    db.session.add(user)
+    db.session.commit()
+    print(User.query.all())
+    print("hello")
     return render_template('home.html', app_data=app_data)
+
 
 @app.route("/admin")
 def show_admin():
@@ -61,11 +84,7 @@ def show_admin():
         return render_template('admin.html', app_data=app_data)
     return redirect('/login')
 
+
 @app.route("/home")
 def home():
     return render_template('home.html', app_data=app_data)
-
-# RUN DEV SERVER
-if __name__ == "__main__":
-    #app.run(HOST, debug=DEBUG)
-    serve(app, host="0.0.0.0", port=8080)
--- src/News-App/wsgi.py
+++ src/News-App/wsgi.py
@@ -1,4 +1,5 @@
 from app import app
-
+from waitress import serve
 if __name__ == "__main__":
-    app.run() 
+    app.run()
+    #serve(app, host="0.0.0.0", port=8080)
