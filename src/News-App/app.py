073af7ee13676a4d4e0735652ef107da894833ec
victor <Victor.PenaAlvarado@student.uantwerpen.be>
2023-03-08 10:10:20 +0100

    post_rss reimplemented

--- src/News-App/app.py
+++ src/News-App/app.py
@@ -1,13 +1,16 @@
 import os
 
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
 from flask.templating import render_template
-from flask import request, session, jsonify, redirect, flash
+from flask import request, session, jsonify, redirect, flash, url_for
 
 from config import config_data
 from DBConnection import DBConnection
 
 from database import *
+
+from RSSCounter import RSSCounter
 
 
 #from waitress import serve
@@ -21,12 +24,18 @@ app_data = dict()
 app_data['app_name'] = config_data['app_name']
 connection = DBConnection(dbname=config_data['dbname'], dbuser=config_data['dbuser'], password="")
 
+db = SQLAlchemy(app)
+
 DEBUG = False
 HOST = "127.0.0.1" if DEBUG else "0.0.0.0"
 
 # TEST USER
 user = {"username": "abc", "password": "xyz"}
 
+
+#Counter for RSS_URL ID
+
+count = RSSCounter()
 
 # REST API
 # See https://www.ibm.com/developerworks/library/ws-restful/index.html
@@ -68,12 +77,19 @@ def show_admin():
 def home():
     return render_template('home.html', app_data=app_data)
 
+
 @app.route("/post_rss", methods= ['POST'])
 def post_rss():
 
 
-    rss =Feed()
+    rss = RSS()
+    rss.rss_url = request.form['feed_url']
+    rss.published_by = request.form['feed_name']
 
+    db.session.add(rss)
+    db.session.commit()
+
+    return (url_for('show_admin'))
 
 # RUN DEV SERVER
 if __name__ == "__main__":
