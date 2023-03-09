60926924628ea1be648cb240b796cb59a648d66f
victor <Victor.PenaAlvarado@student.uantwerpen.be>
2023-03-09 23:16:51 +0100

    error 404 handler implemented (possible changes to html template)

--- src/News-App/app.py
+++ src/News-App/app.py
@@ -1,11 +1,17 @@
 import os
 
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
 from flask.templating import render_template
-from flask import request, session, jsonify, redirect, flash
+from flask import request, session, jsonify, redirect, flash, url_for
 
 from config import config_data
-from DBConnection import DBConnection
+#from DBConnection import DBConnection
+
+#from database import *
+
+from RSSCounter import RSSCounter
+
 
 #from waitress import serve
 
@@ -16,7 +22,9 @@ app = Flask('News-App ')
 app.secret_key = '*^*(*&)(*)(*afafafaSDD47j\3yX R~X@H!jmM]Lwf/,?KT'
 app_data = dict()
 app_data['app_name'] = config_data['app_name']
-connection = DBConnection(dbname=config_data['dbname'], dbuser=config_data['dbuser'], password="")
+#connection = DBConnection(dbname=config_data['dbname'], dbuser=config_data['dbuser'], password="")
+
+#db = SQLAlchemy(app)
 
 DEBUG = False
 HOST = "127.0.0.1" if DEBUG else "0.0.0.0"
@@ -24,6 +32,10 @@ HOST = "127.0.0.1" if DEBUG else "0.0.0.0"
 # TEST USER
 user = {"username": "abc", "password": "xyz"}
 
+
+#Counter for RSS_URL ID
+
+count = RSSCounter()
 
 # REST API
 # See https://www.ibm.com/developerworks/library/ws-restful/index.html
@@ -64,6 +76,28 @@ def show_admin():
 @app.route("/home")
 def home():
     return render_template('home.html', app_data=app_data)
+
+
+@app.route("/post_rss", methods= ['POST'])
+def post_rss():
+
+
+    #rss = RSS()
+    #rss.rss_url = request.form['feed_url']
+    #rss.published_by = request.form['feed_name']
+
+    #db.session.add(rss)
+    #db.session.commit()
+
+    return render_template('admin.html', app_data = app_data)
+
+@app.errorhandler(404)
+def page_not_found(e):
+     return render_template('404.html', app_data = app_data)
+
+@app.errorhandler(403)
+def forbiden_access(e):
+    return render_template('403.html', app_data = app_data)
 
 # RUN DEV SERVER
 if __name__ == "__main__":
