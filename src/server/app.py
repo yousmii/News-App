5990fedd5496fdb57a9d63d13bb50800ea0cc15e
AchrafYndz <achraf.yandouzi@gmail.com>
2023-03-12 14:35:55 +0100

    Make React automatically call backend

--- src/server/app.py
+++ src/server/app.py
@@ -2,7 +2,7 @@ from flask import Flask
 from config import config_data
 
 # INITIALIZE SINGLETON SERVICES
-app = Flask('NewsApp ', static_url_path='', static_folder='./build')
+app = Flask('NewsApp ')
 app.secret_key = '*^*(*&)(*)(*afafafaSDD47j\3yX R~X@H!jmM]Lwf/,?KT'
 app_data = dict()
 app_data['app_name'] = config_data['app_name']
