a60b0ffa2e3d15ae247fb01cee3a42d0f8bfc075
ythienpont <yonah.thienpont@skynet.be>
2023-03-15 11:51:42 +0100

    Listening at 0.0.0.0

--- vite.config.ts
+++ vite.config.ts
@@ -8,7 +8,7 @@ export default defineConfig({
     server: {
         proxy: {
             '/api': {
-                target: 'http://127.0.0.1:5000/',
+                target: 'http://0.0.0.0:5000/',
                 changeOrigin: true
             }
         }
