84d14a581df665d1764c7ffa58ae9485ecdfc2e2
judithher <Judith.Hershko@student.uantwerpen.be>
2023-03-15 16:07:04 +0100

    laatste versie

--- src/server/ConnectDB.py
+++ src/server/ConnectDB.py
@@ -2,8 +2,9 @@
 from datetime import datetime
 from alembic import op
 from flask_sqlalchemy import SQLAlchemy
-from database import User, RSS,NewsSource
+from database import User, RSS, NewsSource
 from sqlalchemy import inspect
+
 
 # from src.server.config import db
 
@@ -18,13 +19,14 @@ class ConnectDB():
 
     def checkRSSExists(self, id):
         return self.db.session.query(RSS.id).filter_by(id=id).first() is not None
-    def checkSourceExisits(self,name:str):
+
+    def checkSourceExisits(self, name: str):
         return self.db.session.query(NewsSource.name).filter_by(name=name).first() is not None
 
-    def column_exists(self,table_name, column_name):
+    def column_exists(self, table_name, column_name):
         inst = inspect(self.db.engine)
-        #attr_names = [c_attr.key for c_attr in inst.mapper.column_attrs]
-        #return attr_names is None
+        # attr_names = [c_attr.key for c_attr in inst.mapper.column_attrs]
+        # return attr_names is None
 
     def addUser(self, cookie, history=""):
         u = User(cookie=cookie, history=history)
@@ -33,11 +35,11 @@ class ConnectDB():
             self.db.session.commit()
         else:
             print("user already in db")
-        #print(User.query.all())
+        # print(User.query.all())
 
     def addRSS(self, rss_url: str, published_by: str):
         if not self.checkSourceExisits(published_by):
-            source=NewsSource(name=published_by)
+            source = NewsSource(name=published_by)
             self.db.session.add(source)
             self.db.session.commit()
         rss = RSS(rss_url=rss_url, source_id=published_by)
@@ -45,5 +47,3 @@ class ConnectDB():
             self.db.session.add(rss)
             self.db.session.commit()
         print(RSS.query.all())
-
-
--- src/server/config.py
+++ src/server/config.py
@@ -12,7 +12,7 @@ def run_app():
     # create the extension
     # INITIALIZE SINGLETON SERVICES
     with app.app_context():
-        #db.create_all(db.engine)
+        # db.create_all(db.engine)
         """
         if table doesn't exists => in python console:
         from src.server.config import app
@@ -29,7 +29,5 @@ def run_app():
         db.create_all()
 
 
-from database import *
-
-if __name__ is '__main__':
+if __name__ == '__main__':
     run_app()
