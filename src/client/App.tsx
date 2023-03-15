aced4ce800a3b4a8a0a7814e982f72f731efe47d
Yousra <yousra.smits@hotmail.com>
2023-03-15 12:01:28 +0100

    Article text fixup

--- src/client/App.tsx
+++ src/client/App.tsx
@@ -1,49 +1,51 @@
-import React, {useState, useEffect} from "react";
+import React, { useState, useEffect } from "react";
 import Header from "./components/Header";
-import styles from "./components/Article.module.scss"
-import axios from 'axios';
+import styles from "./components/Article.module.scss";
+import axios from "axios";
 
 function App() {
-    const [data, setData] = useState([]);
-    const [error, setError] = useState(null);
+  const [data, setData] = useState([]);
+  const [error, setError] = useState(null);
 
-    useEffect(() => {
-        async function fetchData() {
-            try {
-                const response = await axios.get('/api');
-                setData(response.data);
-            } catch (error) {
-                setError(error.response.data.error);
-            }
-        }
-
-        fetchData();
-    }, []);
-
-    if (error) {
-        return (
-            <div>
-                <Header/>
-                <div className={styles.error}>{error}</div>
-            </div>
-        );
+  useEffect(() => {
+    async function fetchData() {
+      try {
+        const response = await axios.get("/api");
+        setData(response.data);
+      } catch (error) {
+        setError(error.response.data.error);
+      }
     }
 
+    fetchData();
+  }, []);
+
+  if (error) {
     return (
-        <div>
-            <Header/>
-            <div className={styles.articles}>
-                {data.map((item, index) => (
-                    <div className={styles.article}>
-                        <a  href={item.link}target={'blank'}>
-                            <img src={item.image} alt={item.title}/>
-                            <h2>{item.title}</h2>
-                        </a>
-                    </div>
-                ))}
-            </div>
-        </div>
+      <div>
+        <Header />
+        <div className={styles.error}>{error}</div>
+      </div>
     );
+  }
+
+  return (
+    <div>
+      <Header />
+      <div className={styles.body}>
+        <div className={styles.articles}>
+          {data.map((item, index) => (
+            <div className={styles.article}>
+              <a href={item.link} target={"blank"}>
+                <img src={item.image} alt={item.title} />
+                <h2>{item.title}</h2>
+              </a>
+            </div>
+          ))}
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default App;
