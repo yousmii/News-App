e06c86cfedb19989505ba0a42d950a704280e1ed
judithher <Judith.Hershko@student.uantwerpen.be>
2023-03-19 16:42:59 +0100

    local settings

--- src/server/sql/local_settings.py
+++ src/server/sql/local_settings.py
@@ -1,17 +1,7 @@
 postgresql={
-    'pguser':'judithhershko',
-    'pgpassword':'',
-    'pghost':'localhost',
-    'pgport':'5432',
-    'pgdb':'alpha',
-}
-
-"""
-postgresql={
     'pguser':'app',
     'pgpassword':'password',
     'pghost':'localhost',
     'pgport':'5432',
     'pgdb':'dbtutor ',
 }
-"""
