255c611e56066bf5f245278cc755ded60b791a3d
AchrafYndz <achraf.yandouzi@gmail.com>
2023-03-20 19:08:51 +0100

    Fix `yarn run build` error

--- src/client/pages/Homepage.tsx
+++ src/client/pages/Homepage.tsx
@@ -1,44 +1,46 @@
-import React, { useEffect, useState } from "react";
-import axios from "axios";
+import React, {useEffect, useState} from "react";
+import axios, {AxiosError} from "axios";
 import styles from "../components/Article.module.scss";
 
 export default function Homepage() {
-  const [data, setData] = useState([]);
-  const [error, setError] = useState(null);
+    const [data, setData] = useState<any[]>([]);
+    const [error, setError] = useState(null);
 
-  useEffect(() => {
-    async function fetchData() {
-      try {
-        const response = await axios.get("/api");
-        setData(response.data);
-      } catch (error) {
-        setError(error.response.data.error);
-      }
+    useEffect(() => {
+        async function fetchData() {
+            try {
+                const response = await axios.get("/api");
+                setData(response.data);
+            } catch (error) {
+                if (error instanceof AxiosError) {
+                    setError(error.response?.data.error);
+                }
+            }
+        }
+
+        fetchData();
+    }, []);
+
+    if (error) {
+        return (
+            <div>
+                <div className={styles.error}>{error}</div>
+            </div>
+        );
     }
 
-    fetchData();
-  }, []);
-
-  if (error) {
     return (
-      <div>
-        <div className={styles.error}>{error}</div>
-      </div>
+        <div>
+            <div className={styles.articles}>
+                {data.map((item, index) => (
+                    <div className={styles.article}>
+                        <a href={item.link} target={"blank"}>
+                            <img src={item.image} alt={item.title}/>
+                            <h2>{item.title}</h2>
+                        </a>
+                    </div>
+                ))}
+            </div>
+        </div>
     );
-  }
-
-  return (
-    <div>
-      <div className={styles.articles}>
-        {data.map((item, index) => (
-          <div className={styles.article}>
-            <a href={item.link} target={"blank"}>
-              <img src={item.image} alt={item.title} />
-              <h2>{item.title}</h2>
-            </a>
-          </div>
-        ))}
-      </div>
-    </div>
-  );
 }
