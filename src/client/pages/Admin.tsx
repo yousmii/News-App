2a9124d91ef48170c87b32a38d2db7f4fad184cb
AchrafYndz <achraf.yandouzi@gmail.com>
2023-04-03 11:02:37 +0200

    Create components for forms, fixing void return issue

--- src/client/pages/Admin.tsx
+++ src/client/pages/Admin.tsx
@@ -1,107 +1,126 @@
 import React, {Component} from "react";
 import styles from "../components/Admin.module.scss";
 
-
-
-
 export default function Admin() {
-  class RSSForm extends Component {
-    handleSubmit = (e : React.ChangeEvent<HTMLFormElement>) => {
-      e.preventDefault();
-      console.log("submitted");
+    return (
+        <div className={styles.container}>
+            <div className={styles.forms}>
+                <RSSForm/>
+                <AdminForm/>
+            </div>
+        </div>
+    );
+}
 
-      const body = e.target.body.value;
+class RSSForm extends Component {
+    handleSubmit = (e: React.ChangeEvent<HTMLFormElement>) => {
+        e.preventDefault();
+        console.log("submitted");
 
-      fetch('post_rss', {
-        method: "POST",
-        body: JSON.stringify(body)
-      }).then((response) => {
-        console.log(response);
-        return response.json(); // do something with response JSON
-      });
-  };
+        const feedName = e.target.feed_name.value;
+        const feedUrl = e.target.feed_url.value;
+
+        const formData = {feed_name: feedName, feed_url: feedUrl};
+
+        fetch('api/post_rss', {
+            method: "POST",
+            body: JSON.stringify(formData),
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        }).then((response) => {
+            console.log(response);
+            return response.json();
+        });
+    };
 
 
     render() {
-    return (
-    <div className={styles.container}>
-      <div className={styles.forms}>
-        <div className={styles.form}>
-          <h1>Add new RSS Feed</h1>
-          <form onSubmit={this.handleSubmit} method="post">
-            <label>Feed URL:</label>
-            <input
-              title="feedurl"
-              type="text"
-              id="feed_url"
-              name="feed_url"
-              required
-            />
-            <br />
-            <label>Feed Name:</label>
-            <input
-              title="feedname"
-              type="text"
-              id="feed_name"
-              name="feed_name"
-              required
-            />
-            <br />
-            <div className={styles.buttons}>
-              <input
-                className={styles.button + " " + styles.add}
-                type="submit"
-                value="Add Feed"
-              />
-              <input
-                className={styles.button + " " + styles.remove}
-                type="submit"
-                value="Delete Feed"
-              />
+        return (
+            <div className={styles.form}>
+                <h1>Add new RSS Feed</h1>
+                <form onSubmit={this.handleSubmit} method="post">
+                    <label>Feed URL:</label>
+                    <input
+                        title="feedurl"
+                        type="text"
+                        id="feed_url"
+                        name="feed_url"
+                        required
+                    />
+                    <br/>
+                    <label>Feed Name:</label>
+                    <input
+                        title="feedname"
+                        type="text"
+                        id="feed_name"
+                        name="feed_name"
+                        required
+                    />
+                    <br/>
+                    <input
+                        className={styles.button + " " + styles.add}
+                        type="submit"
+                        value="Add Feed"
+                    />
+                </form>
             </div>
-          </form>
-        </div>
-        <div className={styles.form}>
-          <h1>Add new Admin</h1>
-          <form method="post">
-            <label>Username:</label>
-            <input
-              title="username"
-              type="text"
-              id="username"
-              name="username"
-              required
-            />
-            <br />
-            <label>Password:</label>
-            <input
-              title="password"
-              type="password"
-              id="password"
-              name="password"
-              required
-            />
-            <br />
-            <div className={styles.buttons}>
-              <input
-                className={styles.button + " " + styles.add}
-                type="submit"
-                value="Add Admin"
-              />
-              <input
-                className={styles.button + " " + styles.remove}
-                type="submit"
-                value="Delete Admin"
-              />
-            </div>
-          </form>
-        </div>
-      </div>
-    </div>
-  );
-}
+        );
     }
-  }
+}
 
+class AdminForm extends Component {
+    handleSubmit = (e: React.ChangeEvent<HTMLFormElement>) => {
+        e.preventDefault();
+        console.log("submitted");
 
+        const adminName = e.target.username.value;
+        const adminPassword = e.target.password.value;
 
+        const formData = {admin_name: adminName, admin_password: adminPassword};
+
+        fetch('api/post_admin', {
+            method: "POST",
+            body: JSON.stringify(formData),
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        }).then((response) => {
+            console.log(response);
+            return response.json();
+        });
+    };
+
+    render() {
+        return (
+            <div className={styles.form}>
+                <h1>Add new Admin</h1>
+                <form onSubmit={this.handleSubmit} method="post">
+                    <label>Username:</label>
+                    <input
+                        title="username"
+                        type="text"
+                        id="username"
+                        name="username"
+                        required
+                    />
+                    <br/>
+                    <label>Password:</label>
+                    <input
+                        title="password"
+                        type="password"
+                        id="password"
+                        name="password"
+                        required
+                    />
+                    <br/>
+                    <input
+                        className={styles.button + " " + styles.add}
+                        type="submit"
+                        value="Add Admin"
+                    />
+                </form>
+            </div>
+        );
+    }
+}
