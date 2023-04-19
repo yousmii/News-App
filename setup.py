573bfe819fb1ff853757256c70c3dee229ef13d2
judithher <Judith.Hershko@student.uantwerpen.be>
2023-04-19 09:29:52 +0200

    setup.py

--- setup.py
+++ setup.py
@@ -2,13 +2,16 @@
 TODO: istall setup.py to fix relative import problems
 pip install -e .
 (-e for editable)
+
 """
+
 from distutils.core import setup
+from setuptools import find_packages
 
 setup(name='News-App',
       version='1.0',
-      description='News-App created for PPBD',
+      description='source of News-App created for PPBD',
       author='Team 2',
-      url='https://www.python.org/sigs/distutils-sig/',
-      packages=['src']
+      url='https://github.com/yousmii/News-App/tree/main/src',
+      packages=find_packages(include=['src', 'src.*'])
      )
