88694287da18ff5971f1c4e22500e9549dc29cfc
yousmii <yousra.smits@hotmail.com>
2023-05-03 01:13:08 +0200

    ... the actual optimisations

--- src/client/pages/Error403.tsx
+++ src/client/pages/Error403.tsx
@@ -1,35 +1,29 @@
-import {useNavigate} from "react-router-dom";
-import React, {useEffect} from "react";
-import {redirect} from "react-router-dom";
+import { useNavigate } from "react-router-dom";
+import React, { useEffect } from "react";
+import { redirect } from "react-router-dom";
 
 export default function Error403() {
+  const navigate = useNavigate();
 
-    const navigate = useNavigate()
+  useEffect(() => {
+    setTimeout(() => {
+      navigate("/");
+    }, 5000);
+  });
 
-    useEffect(()=>{
-    setTimeout(()=>{
-        navigate('/')
-    }, 5000)
-    })
-
-
-    
-
-
-    return (
-        <div style={{
-
-                backgroundImage: "url(/403.jpeg)",
-                backgroundRepeat: "no-repeat",
-                backgroundColor: "black",
-                backgroundPosition: "center",
-                backgroundSize: "contain",
-                width: '100vw',
-                height: '100vh',
-            }}>
-    `
-            </div>
-
-    )
-
+  return (
+    <div
+      style={{
+        backgroundImage: "url(/403.jpeg)",
+        backgroundRepeat: "no-repeat",
+        backgroundColor: "black",
+        backgroundPosition: "center",
+        backgroundSize: "contain",
+        width: "100vw",
+        height: "100vh",
+      }}
+    >
+      `
+    </div>
+  );
 }
