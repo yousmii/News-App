fd7dff25561b758dee4a695076df0ab3a395f1b1
yousmii <yousra.smits@hotmail.com>
2023-05-03 01:54:03 +0200

    Error pages update

--- src/client/pages/Error403.tsx
+++ src/client/pages/Error403.tsx
@@ -1,29 +1,23 @@
 import { useNavigate } from "react-router-dom";
 import React, { useEffect } from "react";
 import { redirect } from "react-router-dom";
+import styles from "../components/Error403.module.scss";
 
 export default function Error403() {
   const navigate = useNavigate();
 
-  useEffect(() => {
-    setTimeout(() => {
-      navigate("/");
-    }, 5000);
-  });
-
   return (
-    <div
-      style={{
-        backgroundImage: "url(/403.jpeg)",
-        backgroundRepeat: "no-repeat",
-        backgroundColor: "black",
-        backgroundPosition: "center",
-        backgroundSize: "contain",
-        width: "100vw",
-        height: "100vh",
-      }}
-    >
-      `
+    <div>
+      <div className={styles.errorcontainer}>
+        <img
+          src="/error403.png"
+          alt="404 Error"
+          className={styles.errorimage}
+        />
+        <a href="/" className={styles.errorlink}>
+          Back to home page
+        </a>
+      </div>
     </div>
   );
 }
--- src/client/pages/Error404.tsx
+++ src/client/pages/Error404.tsx
@@ -1,14 +1,18 @@
 import styles from "../components/Error404.module.scss";
 
 export default function Error404() {
-    return (
-            <div >
-                <div className={styles.errorcontainer}>
-                    <h1 className={styles.errorheading}>Error 404: Page not found</h1>
-                    <p className={styles.errormessage}>The page you are looking for does not exist.</p>
-                    <img src="/error404.png" alt="404 Error" className={styles.errorimage}/>
-                    <a href="/" className={styles.errorlink}>Return to Home</a>
-            </div>
-        </div>
-    )
+  return (
+    <div>
+      <div className={styles.errorcontainer}>
+        <img
+          src="/error404.png"
+          alt="404 Error"
+          className={styles.errorimage}
+        />
+        <a href="/" className={styles.errorlink}>
+          Back to home page
+        </a>
+      </div>
+    </div>
+  );
 }
