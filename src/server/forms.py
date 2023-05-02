f03a06c1f315ab2de7bceb85c3b3ccbaf7ee6a51
Youssef <youssef.elaseri@student.uantwerpen.be>
2023-05-02 18:41:51 +0200

    form password maximum

--- src/server/forms.py
+++ src/server/forms.py
@@ -7,7 +7,7 @@ from src.server.database import User
 class RegisterForm(FlaskForm):
     username = StringField(label='User Name', validators=[Length(min=3, max=30), DataRequired()])
     email_address = StringField('Email Address', validators=[Email(), DataRequired()])
-    password1 = PasswordField(label='Password:', validators=[Length(min=6), DataRequired()])
+    password1 = PasswordField(label='Password:', validators=[Length(min=6, max=30), DataRequired()])
     password2 = PasswordField(label='Confirm Password:', validators=[EqualTo('password1'), DataRequired()])
     submit = SubmitField(label='Create Account')
 
