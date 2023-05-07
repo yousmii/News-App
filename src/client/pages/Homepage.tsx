2c40d2c5ae94fe0a81f2d55640f557cf77a078e6
AchrafYndz <achraf.yandouzi@gmail.com>
2023-05-07 13:56:28 +0200

    Add search bar to homepage

--- src/client/pages/Homepage.tsx
+++ src/client/pages/Homepage.tsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from "react";
-import axios, { AxiosError } from "axios";
+import axios from "axios";
 import styles from "../components/Article.module.scss";
 import moment from "moment";
 
@@ -7,34 +7,74 @@ import Scroller from "../components/InfiteScroller"
 
 export default function Homepage() {
     const [username, setUsername] = useState<string | null>(null);
+    const [searchQuery, setSearchQuery] = useState<string>("");
+    const [searchResults, setSearchResults] = useState<any>(null);
 
-    const user = (event: any) => {
-        event.preventDefault()
-        axios.get('/api/@me', {
-            headers: {
-                'Content-Type': 'application/json'
+    useEffect(() => {
+        const params = new URLSearchParams(window.location.search);
+        const q = params.get('q');
+        if (q) {
+            setSearchQuery(q);
+            handleSearch(q);
+        }
+    }, []);
+
+    const handleSearch = (query: string) => {
+        axios.get('/api/search', {
+            params: {
+                q: query
             }
         })
             .then(response => {
-                if (response.status === 200) {
-                    setUsername(response.data.username)
-                }
-                else {
-                    console.log("Not logged in")
-                }
+                setSearchResults(response.data);
             })
             .catch(error => {
                 console.log(error)
             })
     }
 
+    const handleClear = () => {
+        window.location.href = "/";
+    }
 
-  return (
-      <div>
-          <div className={styles.container}>
-                <Scroller/>
-
-            </div>)
-      </div>
-  );
+    return (
+        <div>
+            <div>
+                <input type="text" value={searchQuery} onChange={(e) => setSearchQuery(e.target.value)} />
+                <button onClick={() => handleSearch(searchQuery)}>Search</button>
+                <button onClick={handleClear}>Clear</button>
+            </div>
+            <div className={styles.container}>
+                {searchResults ?
+                    (
+                        <div className={styles.articles}>
+                            {searchResults.map(({link, image, title, description, pub_date}: {
+                                link: any,
+                                title: any,
+                                image: any,
+                                description: any,
+                                pub_date: any,
+                            }) => {
+                                return (
+                                    <div className={styles.article}>
+                                        <a href={link} target={"blank"} className={styles.article_link}>
+                                            <img className={styles.favicon} height="16" alt={"favicon"} width="16" src={'http://www.google.com/s2/favicons?domain=' + link} />
+                                            <img src={image !== null ? image : 'img.png'} alt={title}/>
+                                            <h2>{title}</h2>
+                                            <p className={styles.description}>{description}</p>
+                                            <p className={styles.time_ago}>{moment(pub_date).fromNow()}</p>
+                                        </a>
+                                    </div>
+                                )
+                            })}
+                        </div>
+                    ) :
+                    (
+                        <Scroller />
+                    )
+                }
+            </div>
+        </div>
+    );
 }
+
