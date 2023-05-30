9f392cc04e38e99592de13ef1730c0af62173aeb
yousmii <yousra.smits@hotmail.com>
2023-05-30 23:29:53 +0200

    Sharbutton cleanup

--- src/client/components/ShareButton.tsx
+++ src/client/components/ShareButton.tsx
@@ -1,20 +1,24 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faFacebook, faTwitter } from "@fortawesome/free-brands-svg-icons";
 import { faCopy } from "@fortawesome/free-solid-svg-icons";
-import styles from './modules/ShareButton.module.scss';
+import styles from "./modules/ShareButton.module.scss";
 
 export const ShareButton = ({ url }) => {
   const [isCopied, setIsCopied] = useState(false);
 
   const shareOnFacebook = () => {
-    const facebookUrl = `https://www.facebook.com/sharer/sharer.php?u=${encodeURIComponent(url)}`;
-    window.open(facebookUrl, '_blank');
+    const facebookUrl = `https://www.facebook.com/sharer/sharer.php?u=${encodeURIComponent(
+      url
+    )}`;
+    window.open(facebookUrl, "_blank");
   };
 
   const shareOnTwitter = () => {
-    const twitterUrl = `https://twitter.com/intent/tweet?url=${encodeURIComponent(url)}`;
-    window.open(twitterUrl, '_blank');
+    const twitterUrl = `https://twitter.com/intent/tweet?url=${encodeURIComponent(
+      url
+    )}`;
+    window.open(twitterUrl, "_blank");
   };
 
   const copyToClipboard = () => {
@@ -28,7 +32,7 @@ export const ShareButton = ({ url }) => {
   };
 
   return (
-    <div>
+    <div className={styles.shareButtons}>
       <button onClick={shareOnFacebook} className={styles.shareButton}>
         <FontAwesomeIcon icon={faFacebook} />
       </button>
